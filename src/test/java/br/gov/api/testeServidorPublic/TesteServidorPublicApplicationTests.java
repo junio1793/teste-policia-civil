8383bd0cd4f8df2a118171de8dd65220ba0bfa03
junio1793 <sjunior863@gmail.com>
2023-09-10 18:26:19 -0300

    first

---
+++ src/test/java/br/gov/api/testeServidorPublic/TesteServidorPublicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.api.testeServidorPublic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteServidorPublicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
